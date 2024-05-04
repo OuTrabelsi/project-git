f4d1a24f994b1f2d8e5af4167dc5172fa41fccc2
Mariem <mariem.hassyaoui@esprit.tn>
2024-05-04 14:04:55 +0100

    Class for fun

---
+++ NotSoSeriousClass.java
@@ -0,0 +1,13 @@
+public class NotSoSeriousClass {
+
+  public static void main(String[] args) {
+    System.out.println(" I am a very serious class. ");
+    System.out.println(" Notice how proper my indentation is?  ");
+    System.out.println("  _____ ");
+    System.out.println(" /     \\");
+    System.out.println("|       |");
+    System.out.println(" \\     / ");
+    System.out.println("  ----- ");
+    System.out.println("  Haha! Gotcha! ");
+  }
+}
